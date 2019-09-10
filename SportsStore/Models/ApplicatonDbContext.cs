ea5d9615a12e40fc6b979f5be9071a4ed1cbc51d
Jordan Ketterling <jordanketterling@outlook.com>
2019-09-10 11:39:53 -0400

    Initial commit.

---
+++ SportsStore/Models/ApplicatonDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+            
+    }
+}
